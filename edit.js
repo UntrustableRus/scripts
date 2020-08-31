6a696baeaf20306d095d0782111ccf3a2d1c7ee7
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2020-08-31 14:58:14 -0400

    Update edit.js

--- edit.js
+++ edit.js
@@ -1,4 +1,4 @@
-if (window.location.hostname == 'scriptcentral.glitch.me' || 'v4.tetretalk.com' || 'tetretalk.com') {
+if (window.location.hostname == 'scriptcentral.glitch.me' || window.location.hostname == 'v4.tetretalk.com') {
 noPayloads();
 } else if (document.body.contentEditable !== 'true') {
 document.body.contentEditable='true'; document.designMode='on'; 
