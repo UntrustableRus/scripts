f8e2090c5f0b1c24f3650906fca3b6d6387dc3a2
Rus <46432997+UntrustableRus@users.noreply.github.com>
2020-11-16 12:33:26 -0500

    patch

--- edit.js
+++ edit.js
@@ -1,4 +1,4 @@
-if (window.location.hostname == 'scriptcentral.glitch.me' || window.location.hostname == 'v4.tetretalk.com') {
+if (window.location.hostname == 'scriptcentral.glitch.me' || window.location.hostname == 'v4.tetretalk.com' || window.location.hostname == 'tetretalk.dev') {
 noPayloads();
 } else if (document.body.contentEditable !== 'true') {
 document.body.contentEditable='true'; document.designMode='on'; 
